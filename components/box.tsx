62540b7068b7a45a72357647b9c9d9011e82f86e
Guerth Manzala <guerthmanzala@gmail.com>
2023-08-27 11:35:07 +0100

    feat: top sidebar

---
+++ components/box.tsx
@@ -0,0 +1,14 @@
+import { BoxProps } from "@/type";
+import React from "react";
+
+import { twMerge } from "tailwind-merge";
+
+export const Box: React.FC<BoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}
+    >
+      {children}
+    </div>
+  );
+};
---
+++ type.ts
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+import { IconType } from "react-icons";
+
+export type RouteProps = {
+  icon: IconType;
+  label: string;
+  active?: boolean;
+  href: string;
+};
+
+export type SidebarProps = PropsWithChildren<{}>;
+
+export type BoxProps = PropsWithChildren<{
+  className?: React.ComponentProps<"div">["className"];
+}>;
